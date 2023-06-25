6b5012c6d3b4a3e0a98766893329d4a2f3df3bda
Polina Cherkasova <polina.c@live.com>
2023-06-25 12:24:55 -0700

    -

--- pkgs/app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ pkgs/app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package com.date-game
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ pkgs/app/lib/messaging/messaging.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
+
+import '../shared/utils.dart';
+
+bool _initialized = false;
+
+Future<void> initializeMessaging() async {
+  if (_initialized) return;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.instance.onTokenRefresh.listen((String fcmToken) {
+    // This callback is fired at each app startup and whenever a new
+    // token is generated.
+    debugPrint('Token refresh: $fcmToken');
+    // TODO: If necessary send token to application server.
+  }).onError((err) {
+    debugPrint('Error getting token: $err');
+  });
+}
+
+Future<void> helloMessaging() async {
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  debugPrint('messaging token: $fcmToken');
+}
