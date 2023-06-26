ef2a48c277275339add44c5df3704c5922b4da8a
Polina Cherkasova <polina.c@live.com>
2023-06-25 21:09:25 -0700

    Update index.js

--- pkgs/fb_functions/functions/index.js
+++ pkgs/fb_functions/functions/index.js
@@ -21,3 +21,22 @@ exports.addmessage = onRequest(async (req, res) => {
     // Send back a message that we've successfully written the message
     res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
+
+// Listens for new messages added to /messages/:documentId/original
+// and saves an uppercased version of the message
+// to /messages/:documentId/uppercase
+exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
+    // Grab the current value of what was written to Firestore.
+    const original = event.data.data().original;
+
+    // Access the parameter `{documentId}` with `event.params`
+    logger.log("Uppercasing", event.params.documentId, original);
+
+    const uppercase = original.toUpperCase();
+
+    // You must return a Promise when performing
+    // asynchronous tasks inside a function
+    // such as writing to Firestore.
+    // Setting an 'uppercase' field in Firestore document returns a Promise.
+    return event.data.ref.set({ uppercase }, { merge: true });
+});
