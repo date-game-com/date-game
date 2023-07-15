2e01e237af789f179edd9d2faa595cb103483ea9
Polina Cherkasova <polina.c@live.com>
2023-07-15 11:09:52 -0700

    -

--- pkgs/app_0/lib/logic/messaging/messaging.dart
+++ pkgs/app_0/lib/logic/messaging/messaging.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-import '../shared/firebase_options.dart';
 import '../shared/utils.dart';
 
 bool _initialized = false;
