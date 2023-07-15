bf28ccfbc5811a9a2ff71f3257a6416c2abbc2f6
Polina Cherkasova <polina.c@live.com>
2023-07-15 11:30:13 -0700

    Update controller.dart

--- pkgs/date_game/lib/logic/auth/controller.dart
+++ pkgs/date_game/lib/logic/auth/controller.dart
@@ -5,10 +5,13 @@ class AuthController {
   static final AuthController instance = AuthController();
 
   AuthController() {
-    FirebaseAuth.instance.authStateChanges().listen((User? newUser) {
-      user.value = newUser;
-    });
+    FirebaseAuth.instance.authStateChanges().listen(_handleUserChanged);
+    FirebaseAuth.instance.userChanges().listen(_handleUserChanged);
+    FirebaseAuth.instance.idTokenChanges().listen(_handleUserChanged);
   }
 
   ValueNotifier<User?> user = ValueNotifier(null);
+  void _handleUserChanged(User? newUser) {
+    user.value = newUser;
+  }
 }
