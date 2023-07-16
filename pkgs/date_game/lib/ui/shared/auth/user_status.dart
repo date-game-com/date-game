81711dc4db965d0c5bfb614037c8b2f2cd6dafb4
Polina Cherkasova <polina.c@live.com>
2023-07-16 15:13:08 -0700

    Update user_status.dart

--- pkgs/date_game/lib/ui/shared/auth/user_status.dart
+++ pkgs/date_game/lib/ui/shared/auth/user_status.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../../logic/auth/controller.dart';
@@ -10,18 +11,62 @@ class UserStatus extends StatelessWidget {
     return ValueListenableBuilder(
       valueListenable: AuthController.instance.user,
       builder: (context, user, child) {
-        final displayName = user?.displayName ?? user?.email;
-        if (displayName == null) {
+        if (user == null) {
           return TextButton(
             onPressed: () {
               AuthController.instance.requestSignIn();
             },
             child: const Text('Log in'),
           );
-        } else {
-          return Text(displayName);
         }
+
+        return _UserDropDown(user: user);
       },
+    );
+  }
+}
+
+class _UserDropDown extends StatelessWidget {
+  const _UserDropDown({required this.user});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    final email = user.email ?? '?';
+
+    return MenuAnchor(
+      //anchorTapClosesMenu: true,
+      builder:
+          (BuildContext context, MenuController controller, Widget? child) {
+        return TextButton(
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          child: const Icon(Icons.face),
+        );
+      },
+      menuChildren: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Signed in as $email'),
+              TextButton(
+                onPressed: () {
+                  AuthController.instance.signOut();
+                },
+                child: const Text('Log out'),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
